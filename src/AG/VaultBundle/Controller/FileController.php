affefa6dadc0909dea0e9d21067ed790816d4e6f
gaillota <manzagregine@gmail.com>
2016-01-08 21:32:40 +0100

    Adding 5 last uploaded files on the left column

--- src/AG/VaultBundle/Controller/FileController.php
+++ src/AG/VaultBundle/Controller/FileController.php
@@ -267,6 +267,26 @@ class FileController extends Controller
     }
 
     /**
+     * @return array
+     * @Template
+     * @Secure(roles="ROLE_ADMIN")
+     */
+    public function getLastAction()
+    {
+        $nbrFile = 5;
+
+        $listFiles = $this->em->getRepository('AGVaultBundle:File')->findBy(array(
+            'owner' => $this->getUser(),
+        ), array(
+            'lastModified' => 'DESC'
+        ), $nbrFile);
+
+        return array(
+            'listFiles' => $listFiles,
+        );
+    }
+
+    /**
      * @param File $file
      * @return array
      * @Template
